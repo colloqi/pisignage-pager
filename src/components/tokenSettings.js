aa30ff8219bac79cedb77dc9e942b382ebc315e9
Navaneeth Rao <navaneetha@ariem2.com>
2016-06-28 11:42:02 +0530

    generate token dialog added

--- src/components/tokenSettings.js
+++ src/components/tokenSettings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Subheader from 'material-ui/Subheader';
 import {List, ListItem} from 'material-ui/List';
 import TextField from 'material-ui/TextField';
+import Dialog from 'material-ui/Dialog';
 import RaisedButton from 'material-ui/RaisedButton'
 
 import FlatButton from 'material-ui/FlatButton';
@@ -28,9 +29,33 @@ let PlayerAdd = React.createClass({
     }
 })
 
-
 export default React.createClass({
+    getInitialState: function(){
+        return({modalOpen: false})
+    },
+    displayModal: function(){
+        console.log('modal open');
+        this.setState({modalOpen: true});
+    },
+    displayModalClose: function(){
+        this.setState({modalOpen: false});
+    },
+    changeRange: function(){
+        console.log('range change');
+    },
     render: function () {
+        let modelActions = [
+            <FlatButton
+                label="Cancel"
+                secondary={true}
+                onTouchTap={this.displayModalClose}
+            />,
+            <RaisedButton
+                label="Ok"
+                primary={true}
+                onTouchTap={this.changeRange}
+            />
+        ]
         return (
             <div>
                 <List>
@@ -38,7 +63,27 @@ export default React.createClass({
                     <ListItem
                         primaryText="Generate Tokens"
                         secondaryText="Creates tokens between 2 values"
-                    />
+                        onTouchTap={this.displayModal}
+                    >
+                        <Dialog 
+                            title="Auto Generate"
+                            actions={modelActions}
+                            modal={false}
+                            open={this.state.modalOpen}
+                            onRequestClose={this.displayModalClose}
+                        >
+                            <TextField
+                                type="number"
+                                hintText=""
+                                floatingLabelText="From"
+                            /><br/>
+                            <TextField
+                                type="number"
+                                hintText=""
+                                floatingLabelText="Till"
+                            />
+                        </Dialog>
+                    </ListItem>
                     <ListItem
                         primaryText="Clear All"
                         secondaryText="Clear All tokens"
